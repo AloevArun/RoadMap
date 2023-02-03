8e59db0df3eabe8aca9f750efbc929fbc6d24a70
Aloev_Arun <aloev.arun@gmail.com>
2023-02-03 14:25:46 +0300

    checkpoint

--- Stepik Advanced/2.2/3.py
+++ Stepik Advanced/2.2/3.py
@@ -1,5 +1,5 @@
 nums = list(map(int, input().split()))
-for num in range(1, len(nums) - 1, 2):
+for num in range(1, len(nums), 2):
     nums[num], nums[num - 1] = nums[num-1], nums[num]
 for num in nums:
     print(num, end=' ')
